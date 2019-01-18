f8d397b7eb5afba680cf9f223577fdabc7445b1c
Jesper Johansson <jesper.johansson@strateg.se>
2019-01-18 13:52:28 +0100

    remove some unwanted rules

--- index.js
+++ index.js
@@ -12,38 +12,8 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
-    // Depricated (remove when it's gone from airbnb's config)
-    'jsx-a11y/label-has-for': 'off',
-    // Maybe enable once it's less buggy
-    'react/destructuring-assignment': 'off',
     'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
-    // Maybe enable once it works better with class fields
-    'react/sort-comp': [
-      'off',
-      {
-        order: [
-          'static-methods',
-          'constructor',
-          'instance-variables',
-          'lifecycle',
-          'everything-else',
-          'rendering',
-        ],
-        groups: {
-          lifecycle: [
-            'state',
-            'componentWillMount',
-            'componentDidMount',
-            'componentWillReceiveProps',
-            'shouldComponentUpdate',
-            'componentWillUpdate',
-            'componentDidUpdate',
-            'componentWillUnmount',
-          ],
-          rendering: ['/^render.+$/', 'render'],
-        },
-      },
-    ],
+    'react/sort-comp': 'off',
   },
 }
